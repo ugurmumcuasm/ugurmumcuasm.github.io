632618dcdf730d212ddf01569d14ec2fee4fc31b
ugurmumcuasm <59837675+ugurmumcuasm@users.noreply.github.com>
2020-01-14 18:20:08 +0300

    Update duyuru.js

--- js/duyuru.js
+++ js/duyuru.js
@@ -1,23 +1,34 @@
 var duyuru = [
     {
         "Başlık":"RANDEVU BİLGİLENDİRME",
-        "Açıklama":"AİLE HEKİMİ MUAYENELERİNİZ İÇİN 182'DEN YADA İNTERNET ÜZERİNDEN MERKEZİ HEKİM RANDEVU SİSTEMİ(MHRS)'DEN AYNI GÜN RANDEVU ALABİLİRSİNİZ."
+        "Açıklama":`AİLE HEKİMİ MUAYENELERİNİZ İÇİN 182'DEN YADA İNTERNET ÜZERİNDEN MERKEZİ HEKİM RANDEVU SİSTEMİ(MHRS)'DEN AYNI GÜN RANDEVU ALABİLİRSİNİZ.`
     },
     {
         "Başlık":"KANSER TARAMASI",
-        "Açıklama":"30-65 YAŞ ARASI BAYAN HASTALARA RAHİM AĞZI KANSER TARAMASI TESTİ YAPILMAKTADIR.15-49 YAŞ ARASI KADIN İZLEMLERİ YAPILMAKTADIR.50-70 YAŞ ARASI TÜM HASTALARA GAİTADA (DIŞKIDA) GİZLİ KAN ARANMASI VE İDRAR TESTİ YAPILMAKTADIR."
+        "Açıklama":`30-65 YAŞ ARASI BAYAN HASTALARA RAHİM AĞZI KANSER TARAMASI TESTİ YAPILMAKTADIR.15-49 YAŞ ARASI KADIN İZLEMLERİ YAPILMAKTADIR.50-70 YAŞ ARASI TÜM HASTALARA GAİTADA (DIŞKIDA) GİZLİ KAN ARANMASI VE İDRAR TESTİ YAPILMAKTADIR.`
     },
     {
         "Başlık":"BEBEK DOSTU",
-        "Açıklama":"Uğur Mumcu Aile Sağlığı Merkezi olarak BEBEK DOSTU olmaya adayız.</br>* Anneler bebeklerini ilk 6 ay sadece anne sütü ile beslemelidirler!</br>* Anne sütü bebekle anne arasında özel sevgi bağı kurulmasını sağlar!</br>* Anne sütü alan bebekler diğer besinlerle beslenen bebeklerden daha zeki olurlar!</br>SÜTSÜZ KADIN YOKTUR.Sütünü vermeyen kadın vardır!BEBEĞİNİZİ SIK SIK EMZİRİN.Anne sütünü arttırmanın tek yolu bebek istedikçe sık sık emzirmektir!BEBEĞE İLK VERİLECEK GIDA,Annenin memesinden ilk gelen ağız sütü olanıdır.Bu süt bebeğin ilk aşısıdır.BEBEĞİNİZİN DOYMADIĞINI DÜŞÜNÜYORSANIZ,İlk yapılması gereken bir sağlık kuruluşuna götürüp tartmaktır.        Yalnız anne sütü ile beslenen bebek bir ayda 500 gr.Ve üzeri kilo alıyorsa anne sütü yeterlidir.Ek besine ihtiyaç yoktur.BEBEĞİNİZİN ALTINI SIK SIK AÇIP TEMİZLEYİN.Unutulmamalıdır ki anne ile doyan bebek normal günde 4-6 kez bezini ıslatır. SAĞLIKLI VE MUTLU BİR BEBEĞE SAHİP OLMAK İÇİN;Onu ilk 6 ay sadece anne sütü ile besleyip         6 aydan sonra uygun ek gıdalar başlayarak 2 yaşına kadar emzirmeye devam ediniz."
+        "Açıklama":`Uğur Mumcu Aile Sağlığı Merkezi olarak BEBEK DOSTU olmaya adayız.
+        </br>* Anneler bebeklerini ilk 6 ay sadece anne sütü ile beslemelidirler!
+        </br>* Anne sütü bebekle anne arasında özel sevgi bağı kurulmasını sağlar!
+        </br>* Anne sütü alan bebekler diğer besinlerle beslenen bebeklerden daha zeki olurlar!
+        </br>SÜTSÜZ KADIN YOKTUR.Sütünü vermeyen kadın vardır!
+        </br>BEBEĞİNİZİ SIK SIK EMZİRİN.Anne sütünü arttırmanın tek yolu bebek istedikçe sık sık emzirmektir!
+        </br>BEBEĞE İLK VERİLECEK GIDA,Annenin memesinden ilk gelen ağız sütü olanıdır.Bu süt bebeğin ilk aşısıdır.
+        </br>BEBEĞİNİZİN DOYMADIĞINI DÜŞÜNÜYORSANIZ,İlk yapılması gereken bir sağlık kuruluşuna götürüp tartmaktır.
+        </br>        Yalnız anne sütü ile beslenen bebek bir ayda 500 gr.Ve üzeri kilo alıyorsa anne sütü yeterlidir.Ek besine ihtiyaç yoktur.
+        </br>BEBEĞİNİZİN ALTINI SIK SIK AÇIP TEMİZLEYİN.Unutulmamalıdır ki anne ile doyan bebek normal günde 4-6 kez bezini ıslatır. 
+        </br>SAĞLIKLI VE MUTLU BİR BEBEĞE SAHİP OLMAK İÇİN;Onu ilk 6 ay sadece anne sütü ile besleyip 
+        </br>        6 aydan sonra uygun ek gıdalar başlayarak 2 yaşına kadar emzirmeye devam ediniz.`
     },
     {
         "Başlık":"DUYURU BAŞLIĞI",
-        "Açıklama":"Açıklama,içeriği"
+        "Açıklama":`Açıklama,içeriği`
     },
     {
         "Başlık":"DUYURU BAŞLIĞI",
-        "Açıklama":"Açıklama,içeriği"
+        "Açıklama":`Açıklama,içeriği`
     },
     {
         "Başlık":"DUYURU BAŞLIĞI",
