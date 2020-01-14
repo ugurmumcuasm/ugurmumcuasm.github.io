e57ffea98ed2c16cad9cd28dfa2a8d6c85c7d89d
ugurmumcuasm <59837675+ugurmumcuasm@users.noreply.github.com>
2020-01-14 11:24:44 +0300

    Update duyuru.js

--- js/duyuru.js
+++ js/duyuru.js
@@ -1,7 +1,7 @@
 var duyuru = [
     {
-        "Başlık":"DUYURU BAŞLIĞI",
-        "Açıklama":"Açıklama,içeriği"
+        "Başlık":"RANDEVU BİLGİLENDİRME",
+        "Açıklama":"AİLE HEKİMİ MUAYENELERİNİZ İÇİN 182'DEN YADA İNTERNET ÜZERİNDEN MERKEZİ HEKİM RANDEVU SİSTEMİ(MHRS)'DEN AYNI GÜN RANDEVU ALABİLİRSİNİZ."
     },
     {
         "Başlık":"DUYURU BAŞLIĞI",
