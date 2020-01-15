694d3be78d13153358f8f9d295ec93dca948e0be
ugurmumcuasm <59837675+ugurmumcuasm@users.noreply.github.com>
2020-01-15 11:33:19 +0300

    Update duyuru.js

--- js/duyuru.js
+++ js/duyuru.js
@@ -50,13 +50,20 @@ var duyuru = [
     {
         "Başlık":"DUYURU BAŞLIĞI",
         "Açıklama":"Açıklama,içeriği"
-    },{
+    },
+    {
         "Başlık":"DUYURU BAŞLIĞI",
         "Açıklama":"Açıklama,içeriği"
-    },{
+    },
+    {
         "Başlık":"DUYURU BAŞLIĞI",
         "Açıklama":"Açıklama,içeriği"
-    },{
+    },
+    {
+        "Başlık":"DUYURU BAŞLIĞI",
+        "Açıklama":"Açıklama,içeriği"
+    },
+     {
         "Başlık":"DUYURU BAŞLIĞI",
         "Açıklama":"Açıklama,içeriği"
     }
