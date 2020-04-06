67d25cd59e528fd84b883db5e83c05dc83c8a7ba
ugurmumcuasm <59837675+ugurmumcuasm@users.noreply.github.com>
2020-04-06 23:59:04 +0300

    Update duyuru.js

--- js/duyuru.js
+++ js/duyuru.js
@@ -50,6 +50,10 @@ var duyuru = [
     {
         "Başlık":"GÜNCEL CORONAVİRÜS COVID19 BİLGİLENDİRME",
         "Açıklama":`<a href="https://hsgm.saglik.gov.tr/tr/covid19" target="_blank">SAĞLIK BAKANLIĞI HALK SAĞLIĞI GENEL MÜDÜRLÜĞÜNÜN GÜNCEL CORONAVİRÜS COVID19 BİLGİLERİNE ULAŞMAK İÇİN TIKLAYINIZ</a>`
+    },
+    {
+        "Başlık":"Günlük Vaka Sayıları",
+        "Açıklama":`<a href="https://covid19bilgi.saglik.gov.tr/tr/gunluk-vaka" target="_blank">SAĞLIK BAKANLIĞI Günlük Vaka Sayıları BİLGİLERİNE ULAŞMAK İÇİN TIKLAYINIZ</a>`
     }
     
 ]
