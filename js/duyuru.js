ed5dac3c0041eadea07aa43863652b1913149d74
ugurmumcuasm <59837675+ugurmumcuasm@users.noreply.github.com>
2025-02-05 09:38:07 +0300

    Update duyuru.js

--- js/duyuru.js
+++ js/duyuru.js
@@ -52,66 +52,6 @@ duyuru = [duyuru = [
         "Açıklama":`<a href="https://hsgm.saglik.gov.tr/tr/covid19" target="_blank">SAĞLIK BAKANLIĞI HALK SAĞLIĞI GENEL MÜDÜRLÜĞÜNÜN GÜNCEL CORONAVİRÜS COVID19 BİLGİLERİNE ULAŞMAK İÇİN TIKLAYINIZ</a>`
     },
     {
-        "Başlık":"Günlük COVID-19 Vaka Sayıları",
-        "Açıklama":`<a href="https://covid19.saglik.gov.tr" target="_blank">SAĞLIK BAKANLIĞI Günlük Vaka Sayıları BİLGİLERİNE ULAŞMAK İÇİN TIKLAYINIZ</a>`
-    }
-
-]
-    {
-        "Başlık":"RANDEVU BİLGİLENDİRME",
-        "Açıklama":`AİLE HEKİMİ MUAYENELERİNİZ İÇİN 182'DEN YADA İNTERNET ÜZERİNDEN MERKEZİ HEKİM RANDEVU SİSTEMİ(MHRS)'DEN AYNI GÜN RANDEVU ALABİLİRSİNİZ.RANDEVUSUZ HASTALARA SABAH SAAT 10:00-12:00 VE ÖĞLENDEN SONRA 15:00-16:00 SAATLERİ ARASINDA BAKILMAKTADIR.`
-    },
-    {
-        "Başlık":"KANSER TARAMASI",
-        "Açıklama":`30-65 YAŞ ARASI BAYAN HASTALARA RAHİM AĞZI KANSER TARAMASI TESTİ YAPILMAKTADIR.</br>15-49 YAŞ ARASI KADIN İZLEMLERİ YAPILMAKTADIR.</br>50-70 YAŞ ARASI TÜM HASTALARA GAİTADA (DIŞKIDA) GİZLİ KAN ARANMASI TESTİ YAPILMAKTADIR.</br>30-65 YAŞ ARASI BAYAN HASTALARA SMEAR TESTİ YAPILMAKTADIR.`
-    },
-    {
-        "Başlık":"BEBEK DOSTU",
-        "Açıklama":`Uğur Mumcu Aile Sağlığı Merkezi olarak BEBEK DOSTU dur.
-        </br>* Anneler bebeklerini ilk 6 ay sadece anne sütü ile beslemelidirler!
-        </br>* Anne sütü bebekle anne arasında özel sevgi bağı kurulmasını sağlar!
-        </br>* Anne sütü alan bebekler diğer besinlerle beslenen bebeklerden daha zeki olurlar!
-        </br>SÜTSÜZ KADIN YOKTUR.Sütünü vermeyen kadın vardır!
-        </br>BEBEĞİNİZİ SIK SIK EMZİRİN.Anne sütünü arttırmanın tek yolu bebek istedikçe sık sık emzirmektir!
-        </br>BEBEĞE İLK VERİLECEK GIDA,Annenin memesinden ilk gelen ağız sütü olanıdır.Bu süt bebeğin ilk aşısıdır.
-        </br>BEBEĞİNİZİN DOYMADIĞINI DÜŞÜNÜYORSANIZ,İlk yapılması gereken bir sağlık kuruluşuna götürüp tartmaktır.
-        </br>        Yalnız anne sütü ile beslenen bebek bir ayda 500 gr.Ve üzeri kilo alıyorsa anne sütü yeterlidir.Ek besine ihtiyaç yoktur.
-        </br>BEBEĞİNİZİN ALTINI SIK SIK AÇIP TEMİZLEYİN.Unutulmamalıdır ki anne ile doyan bebek normal günde 4-6 kez bezini ıslatır. 
-        </br>SAĞLIKLI VE MUTLU BİR BEBEĞE SAHİP OLMAK İÇİN;Onu ilk 6 ay sadece anne sütü ile besleyip 
-        </br>        6 aydan sonra uygun ek gıdalar başlayarak 2 yaşına kadar emzirmeye devam ediniz.`
-    },
-    {
-        "Başlık":"DUYURU BAŞLIĞI",
-        "Açıklama":`KAN ALIMI HAFTANIN HER GÜNÜ 08:30-10:00 SAATLERİ ARASINDA RANDEVU ALINARAK YAPILMAKTADIR.SONUÇLAR 1(BİR) GÜN SONRA ÇIKMAKTADIR.`
-    },
-    {
-        "Başlık":"UĞUR MUMCU ASM  EMZİRME POLİTİKAMIZ",
-        "Açıklama":`
-</br>1.Aile Sağlığı Merkezimizde anne sütünün önemi, emzirmenin anne ve bebek sağlığı yönünden yararları ve tüm annelerin emzirmeye teşvik edilmesi amacıyla yazılı bir emzirme politikası oluşturulmuştur. Kurumumuz çalışanları tarafından bu politikanın uygulanmasının sürekliliği sağlanmaktadır.
-</br>2. Aile Sağlığı Merkezimizde çalışanlarımız anne sütü ve emzirme konularında eğitim almıştır. Bu eğitimler belli aralıklarda güncellenmektedir.
-</br>3.Kurumumuza başvuran ve ev gezilerinde tespit edilen tüm gebelere anne sütü ve emzirme ile ilgili eğitimler yapılmaktadır.
-</br>4.Doğumdan hemen sonra , bebeğe hiçbir besin verilmeksizin, bebeğin memeye tutulması ve emzirmenin başlatılması ve ilk 6 ay sadece anne sütü verilmesi altıncı ayın bitiminden sonra  ek gıdalarla birlikte iki yaşa kadar anne sütü verilmesine devam edilmesi konusunda anneler desteklenmelidir.
-</br>5.Lohusalık döneminde tüm annelere, anne sütü ve emzirmenin yararları, doğru emzirme teknikleri, emzirmenin sürdürülmesi, sütün sağılması ve saklaması, sağılmış sütün bebeklere verilmesi  vb. konularda bilgi verilmektedir.
-</br>6.Kurumumuzda emzirme sıklığı ve süresi konusunda herhangi bir kısıtlanma uygulanmaz. Emzirmenin bebeğin isteğine bağlı olduğu, gündüz ve gece devam etmesi gerektiği ve bebek her istediğinde emzirilmesinin yararları anneye aktarılmaktadır.
-</br>7.Tıbbi zorunluluk olmadığı sürece bebeklere ilk 6 aya sadece anne sütü verilmelidir. 6 aydan sonra ek gıdalarla birlikte 2 yaşına kadar anne sütü verilmesi tavsiye edilmektedir.
-</br>8.Emzirmenin desteklenmesi ve sürdürülmesi konularında bölgemizdeki diğer kurum , kuruluş, dernek, merkez, okul, sağlık kurumları vb. ile  işbirliği yapılarak ortak etkinlikler düzenlenmektedir.
-</br>9.Kurumumuzun her sahasında Uluslararası mama kodu uygulamaları benimsenmekte ve uygulanmaktadır. 
-</br>10.Kurumumuzda anne sütü dışında muadil materyalin reklamlarına ilişkin poster, afiş ve numunelerinin bulunmasına ve kullanımına izin verilmemektedir.
-</br>11.Daha zeki ve sağlıklı nesillere sahip olabilmek için anne sütünün önemi her aşamada vurgulanmaktadır.
-</br>12.Kurumumuzdan hizmet alırken yararlanabileceğiniz, emzirme bölümümüz bulunmaktadır.`
-    },
-    {
-        "Başlık":"ERİŞKİN AŞILAMA",
-        "Açıklama":"65 YAŞ ÜSTÜ TÜM HASTALARA VE 65 YAŞ ALTI OLUP KOAH, DİYABET HASTALIĞI, KANSER TEDAVİSİ GÖREN HASTALARA AİLE HEKİMLERİNCE KPA-13 (ZATÜRRE AŞISI) UYGULANMAKTADIR.KONUYLA İLGİLİ OLARAK AİLE HEKİMİNİZE VEYA HEMŞİRENİZE BAŞ VURABİLİRSİNİZ."
-   
-    },
-    {
-        "Başlık":"GÜNCEL CORONAVİRÜS COVID19 BİLGİLENDİRME",
-        "Açıklama":`<a href="https://hsgm.saglik.gov.tr/tr/covid19" target="_blank">SAĞLIK BAKANLIĞI HALK SAĞLIĞI GENEL MÜDÜRLÜĞÜNÜN GÜNCEL CORONAVİRÜS COVID19 BİLGİLERİNE ULAŞMAK İÇİN TIKLAYINIZ</a>`
-    },
-    {
-        "Başlık":"Günlük COVID-19 Vaka Sayıları",
-        "Açıklama":`<a href="https://covid19.saglik.gov.tr" target="_blank">SAĞLIK BAKANLIĞI Günlük Vaka Sayıları BİLGİLERİNE ULAŞMAK İÇİN TIKLAYINIZ</a>`
     }
 
 ]
